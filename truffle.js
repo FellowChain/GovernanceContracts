7b307f0fa6ed628087515960c74f94860dd08543
Adam skrodzki <adamskrodzki@gmail.com>
2018-06-24 10:46:05 +0200

    ropsten and rinkeby config added

--- truffle.js
+++ truffle.js
@@ -13,11 +13,13 @@ var HDWalletProvider = require("truffle-hdwallet-provider");
 
 
 var infuraRinkebyUrl = secretData.INFURA_RINKEBY_URL;
+var infuraRopstenUrl = secretData.INFURA_RINKEBY_URL.replace("rinkeby","ropsten");
 var sokolUrl = secretData.SOKOL_NETWORK_URL;
 var infuraMainUrl = secretData.CORE_NETWORK_URL;
 var coreUrl = secretData.CORE_NETWORK_URL;
 var mnemonic = secretData.SECRET_MNEMONIC;
 var providerRinkeby = new HDWalletProvider(mnemonic, infuraRinkebyUrl);
+var providerRopsten = new HDWalletProvider(mnemonic, infuraRopstenUrl);
 var providerMain = new HDWalletProvider(mnemonic, infuraMainUrl);
 var providerSokol = new HDWalletProvider(mnemonic, sokolUrl);
 
@@ -42,7 +44,13 @@ module.exports = {
     rinkeby: {
       provider: providerRinkeby,
       network_id: 4, // eslint-disable-line camelcase
-      gasPrice: "50000000000",
+      gasPrice: "1000000000",
+      gas: 5000000,
+    },
+    ropsten: {
+      provider: providerRopsten,
+      network_id: 3, // eslint-disable-line camelcase
+      gasPrice: "1000000000",
       gas: 5000000,
     },
     sokol: {
